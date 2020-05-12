63d0b84a4bbd4a8d9ef6a63fc919cc59ab58c890
hanssenstijn <hanssenstijn@gmail.com>
2020-05-12 19:38:54 +0200

    nodejs tutorial

---
+++ callbacks/main.js
@@ -0,0 +1,10 @@
+// Non-blocking code example 
+//  shows that the program does not wait for file reading and proceeds to print "Program Ended" and at the same time, the program without blocking continues reading the file.
+var fs = require("fs");
+
+fs.readFile('input.txt', function (err, data) {
+   if (err) return console.error(err);
+   console.log(data.toString());
+});
+
+console.log("Program Ended");
